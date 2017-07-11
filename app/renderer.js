86f26a1e2cc896da1e73dce3f64c4210b7a9f44d
Mads Nedergaard <nedergaardmads@gmail.com>
2017-07-11 09:13:08 +0200

    More work on app

--- app/renderer.js
+++ app/renderer.js
@@ -21,11 +21,13 @@ app({
    	source: null,
    	selectedShow: null,
    	selectedEpisode: null,
-   	podcasts: [],
-   	isFetching: false
+   	podcasts: {},
+   	isFetching: false,
+   	online: false
   },
   view: (state, actions) => (
-		h('section', {class: 'beats'}, [
+		h('section', {class: 'app'}, [
+//			h('p', {class: 'offline-message'}, 'No internet connection'),
 			// Play button
 	  	h(
 	  		'button', 
@@ -46,20 +48,26 @@ app({
 				}, 
 				state.playing
 			),
+			h('p', null, state.isFetching? 'loading...' : null),
+			h('section',  {class: 'flexy'}, [
 			// Beats
-			h('div', null, [
+			h('div', {class: 'col'}, [
 				h('h2', {onclick: () => actions.toggle()}, 'Beats'),
-				 h('div', {class: 'list '}, Object.keys(state.podcasts).map(s =>
-				 	h('button', {class: s === state.selectedShow ? 'button item selected' : 'button item',  onclick: () => actions.loadEpisodes}, s)
+				 h('div', {class: 'list '}, Object.keys(state.podcasts).sort().map(s =>
+				 	h('button', {class: s === state.selectedShow ? 'button item selected' : 'button item',  onclick: () => actions.selectShow(s)}, s)
 				 ))
 
 			]),
 			// Episodes
-			h('div', null, [
+			h('div', {class: 'col'}, state.selectedShow ? [
 				h('h2', null, 'Episodes'),
-				// h('div', {class: 'list '}, state.podcasts.map(p => 
-				// 	h('button', {class: p.title === state.selectedEpisode ? 'button item selected' : 'button item',  onclick: () => actions.play(p)}, p.title)
-				// ))
+				h('div', {class: 'list '}, state.podcasts[state.selectedShow].length > 0 ? 
+					state.podcasts[state.selectedShow].map(p => 
+				 		h('button', {class: p.title === state.selectedEpisode ? 'button item selected' : 'button item',  onclick: () => actions.play(p)}, p.title)
+				 	)
+				: h('p', null, !state.isFetching ? 'no episodes available' : '')
+				)
+			] : null),
 			])
 		])
   ),
@@ -78,150 +86,142 @@ app({
       podcasts: podcasts || [],
       isFetching
     }),
-    loadEpisodes: (state, actions, show) => {
-			fetch(`http://heartbeats.dk/wp-json/posts?type=podcast&filter[series]=Beats&filter[series]=${show.title}`)
-      .then(episodes => episodes.json())
-      .then(episodes => episodes.map(podcast => ({
-        title: podcast.title.replace(/&#038;/g, '&'),
-        source: podcast.post_meta.audio_file
-	  	  }))
-    	)
-      .then(podcasts => actions.updatePodcasts({ podcasts, isFetching: false }))
+    updateEpisodes: (state, actions, { episodes, isFetching}) => {
+    	if (episodes) {
+    		state.podcasts[state.selectedShow] = episodes;
+    	}
+    	state.isFetching = isFetching;
+    	return state;
     },
-    logState: (state) => ({state})
-  },
-  events: {
-  	loaded: (state, actions) => {
+    updateSelected: (state, actions, {selectedShow, selectedEpisode}) => {
+    	
+    	return {
+    	selectedShow: selectedShow ? selectedShow : state.selectedShow,
+    	selectedEpisode: selectedEpisode ? selectedEpisode : state.selectedEpisode
+  	  };    	
+  	},
+  	selectShow: (state, actions, data, emit) => {
+  		actions.updateSelected({selectedShow: data});
+  		if(state.podcasts[data].length === 0) {
+	  		actions.loadEpisodes();
+  		}
+  	},
+  	loadShows: (state, actions) => {
   		actions.updatePodcasts({isFetching: true});
 		  fetch(`http://heartbeats.dk/wp-admin/admin-ajax.php?action=podcast_series`)
       .then(response => response.json())
-       .then(data => data.beats.series.reduce((o, key) => Object.assign(o, {[key]: []}), {})
-       )
-      ////.replace(/&#038;/g, '&')
-      .then(podcasts => actions.updatePodcasts({ podcasts, isFetching: false }))
-  		// actions.updatePodcasts({isFetching: true});
-			 // fetch(`http://heartbeats.dk/wp-json/posts?type=podcast&filter[series]=Beats&filter[series]=Varme Rytmer`)
-			 //        .then(podcasts => podcasts.json())
-			 //        .then(podcasts => podcasts.map(podcast => ({
-		  //           title: podcast.title.replace(/&#038;/g, '&'),
-		  //           source: podcast.post_meta.audio_file
-		  //       	  }))
-		  //       	)
-			 //        .then(podcasts => actions.updatePodcasts({ podcasts, isFetching: false }))
+      .then(data => data.beats.series.reduce((o, key) => Object.assign(o, {[key]: []}), {}))
+      .then(podcasts => actions.updatePodcasts({ podcasts, isFetching: false }));
+  	},
+
+    loadEpisodes: (state, actions) => {
+  		actions.updateEpisodes({isFetching: true});
+			fetch(`http://heartbeats.dk/wp-json/posts?type=podcast&filter[series]=Beats&filter[series]=${state.selectedShow}`)
+      .then(episodes => episodes.json())
+      .then(episodes => episodes.length > 0 ? episodes.map(podcast => ({
+        title: cleanString(podcast.title),
+        source: podcast.post_meta.audio_file
+	  	  })) : null
+    	)
+      .then(episodes => actions.updateEpisodes({ episodes, isFetching: false }))
+    },
+  },
+  events: {
+  	loaded: (state, actions) => {
+  		actions.loadShows();
   	},
   },
-    mixins: [Logger]
+   // mixins: [Logger]
 });
 
-
-/*
+function cleanString(string) {
+	var cleanString = string
+		.replace(/&#038;/g, '&')
+		.replace(/&amp;/g, '&')
+		.replace(/&#8211;/g, '–')
+		.replace(/&#8217;/g, '\'')
+	;
+	return cleanString;
+}
 var audio = document.getElementById('audioplayer');
-var audioWrapper = document.querySelector('.player');
-var playButton = document.querySelector('.play-button');
-var offlineMessage = document.querySelector('.offline-message');
-
-var beatsWrapper = document.querySelector('.beats');
-var playBeatsButton = document.getElementsByClassName('episode');
-
-
 // Initial check to see if its online
 if(navigator.onLine) {
 	console.log('navigator onLine');
-	audioWrapper.classList.toggle('hidden');	
-	//beatsWrapper.classList.toggle('hidden');	
 }
 
-// https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Media_events
-audio.addEventListener('playing', function(){
-	console.log('Playing');
-});
+// // https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Media_events
+// audio.addEventListener('playing', function(){
+// 	console.log('Playing');
+// });
 
-audio.addEventListener('pause', function(){
-	console.log('Pause');
-});
+// audio.addEventListener('pause', function(){
+// 	console.log('Pause');
+// });
 
-audio.addEventListener('play', function(){
-	console.log('Play');
-});
+// audio.addEventListener('play', function(){
+// 	console.log('Play');
+// });
 
-audio.addEventListener('ended', function(){
-	console.log('Ended');
-});
+// audio.addEventListener('ended', function(){
+// 	console.log('Ended');
+// });
 
-audio.addEventListener('suspend', function(){
-	console.log('Suspend');
-});
+// audio.addEventListener('suspend', function(){
+// 	console.log('Suspend');
+// });
 
-audio.addEventListener('stalled', function(){
-	console.log('stalled');
-});
+// audio.addEventListener('stalled', function(){
+// 	console.log('stalled');
+// });
 
-audio.addEventListener('error', function(err){
-	console.log('error: ');
-	console.log(err);
-	if (navigator.onLine) {
-			if (!audio.paused) {
-				audio.pause();
-			}
-	} else {
-		offlineMessage.classList.toggle('hidden');
-	}
-});
+// audio.addEventListener('error', function(err){
+// 	console.log('error: ');
+// 	console.log(err);
+// 	// if (navigator.onLine) {
+// 	// 		if (!audio.paused) {
+// 	// 			audio.pause();
+// 	// 		}
+// 	// } else {
+// 	// 	offlineMessage.classList.toggle('hidden');
+// 	// }
+// });
 
 
 window.addEventListener('online',  function(){
 	console.log('online');
-	var source = audio.src;
-	audio.src = '';
-	audio.src = source;
-	offlineMessage.classList.toggle('hidden');
-	audioWrapper.classList.toggle('hidden');
+	// var source = audio.src;
+	// audio.src = '';
+	// audio.src = source;
+	// offlineMessage.classList.toggle('hidden');
+	// audioWrapper.classList.toggle('hidden');
 	//beatsWrapper.classList.toggle('hidden');
 });
 window.addEventListener('offline',  function(){
 	console.log('offline');
-	if (!audio.paused) {
-		toggleRadio(audio, playButton); // Stop	
-	}
-	offlineMessage.classList.toggle('hidden');
-	audioWrapper.classList.toggle('hidden');
+	// if (!audio.paused) {
+	// 	toggleRadio(audio, playButton); // Stop	
+	// }
+	// offlineMessage.classList.toggle('hidden');
+	// audioWrapper.classList.toggle('hidden');
 	//beatsWrapper.classList.toggle('hidden');
 });
 
 
-var playBeat = function() {
-    var source = this.getAttribute("data-src");
-    console.log(source);
-  	audio.src = source;
-  	toggleRadio(audio, playButton);
-};
-
-for (var i = 0; i < playBeatsButton.length; i++) {
-    playBeatsButton[i].addEventListener('click', playBeat, false);
-}
-
-
-
-// On play-button
-playButton.addEventListener('click', function () { 
-	toggleRadio(audio, playButton);
-});
-
 // On keyboard play-media-key
 ipcRenderer.on('MediaPlayPause', function () {
-  toggleRadio(audio, playButton);
+	var audio = document.getElementById('audioplayer');
+  toggleRadio(audio);
 });
 
 
-function toggleRadio(audio, button) {
-	button.classList.toggle('playing');
+function toggleRadio(audio) {
+	//button.classList.toggle('playing');
 	if (audio.paused) {
 		audio.play();
-		button.innerText = 'Pause';
+		//button.innerText = 'Pause';
 	} else {
 		audio.pause();
-		button.innerText = 'Play';
+		//button.innerText = 'Play';
 	}
 	return true;
 }
-*/
