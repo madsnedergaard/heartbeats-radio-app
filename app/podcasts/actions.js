58f65c091fec4b512f58cf4a1bd01ad3d6b56aae
Mads Nedergaard <nedergaardmads@gmail.com>
2018-01-05 08:24:12 +0100

    Gathered actiontypes

--- app/podcasts/actions.js
+++ app/podcasts/actions.js
@@ -1,12 +1,24 @@
 import * as Api from '../utils/Api';
 
+// Action types
+export const TOGGLE_AUDIO = 'TOGGLE_AUDIO';
 export const SELECT_SERIES = 'SELECT_SERIES';
+export const SELECT_EPISODE = 'SELECT_EPISODE';
+export const FETCHING_EPISODES = 'FETCHING_EPISODES';
+export const FETCHING_EPISODES_FAILED = 'FETCHING_EPISODES_FAILED';
+export const FETCHING_EPISODES_SUCCEEDED = 'FETCHING_EPISODES_SUCCEEDED';
+export const FETCHING_SERIES = 'FETCHING_SERIES';
+export const FETCHING_SERIES_FAILED = 'FETCHING_SERIES_FAILED';
+export const FETCHING_SERIES_SUCCEEDED = 'FETCHING_SERIES_SUCCEEDED';
+export const SET_FETCHING_STATUS = 'SET_FETCHING_STATUS';
+
+// Action creators
+
 export const selectSeries = (seriesId) => ({
   type: SELECT_SERIES,
   selectedShow: seriesId
 });
 
-export const SELECT_EPISODE = 'SELECT_EPISODE';
 export const selectEpisode = (episodeId, source) => ({
   type: SELECT_EPISODE,
   selectedEpisode: episodeId,
@@ -14,19 +26,10 @@ export const selectEpisode = (episodeId, source) => ({
   isPlaying: true
 });
 
-export const TOGGLE_AUDIO = 'TOGGLE_AUDIO';
 export const toggleAudio = () => ({
   type: TOGGLE_AUDIO
 });
 
-
-// Fetch series
-export const FETCHING_SERIES = 'FETCHING_SERIES';
-export const FETCHING_SERIES_FAILED = 'FETCHING_SERIES_FAILED';
-export const FETCHING_SERIES_SUCCEEDED = 'FETCHING_SERIES_SUCCEEDED';
-export const SET_FETCHING_STATUS = 'SET_FETCHING_STATUS';
-
-// Action creators
 export const fetchingSeries = () => ({
   type: FETCHING_SERIES
 });
@@ -40,12 +43,6 @@ export const fetchSeries = () => dispatch => {
   Api.fetchAllSeries((json) => dispatch(fetchingSeriesSucceeded(json)));
 };
 
-
-export const FETCHING_EPISODES = 'FETCHING_EPISODES';
-export const FETCHING_EPISODES_FAILED = 'FETCHING_EPISODES_FAILED';
-export const FETCHING_EPISODES_SUCCEEDED = 'FETCHING_EPISODES_SUCCEEDED';
-
-// Action creators
 export const fetchingEpisodes = () => ({
   type: FETCHING_EPISODES
 });
@@ -60,7 +57,6 @@ export const fetchEpisodes = (seriesId) => dispatch => {
 };
 
 
-
-//export function setFetchingStatus(status) {
+// export function setFetchingStatus(status) {
 //  return { type: SET_FETCHING_STATUS, status };
-//}
+// }
