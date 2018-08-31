242ab11752374c3025d9e8daa6072eec62e22c6b
Mads Nedergaard <nedergaardmads@gmail.com>
2018-08-31 09:37:47 +0200

    Changed API to fetch data from a JSON file, since the Heartbearts API has been made private

--- app/utils/Api.js
+++ app/utils/Api.js
@@ -1,19 +1,31 @@
 // Series
-export const fetchAllSeries = (next) => {
-  fetch("http://heartbeats.dk/wp-json/wp/v2/series?parent=1284&per_page=100")
+
+/*
+ Since the Heartbeats API has been closed off, I have modified the API calls to fetch data from a JSON file.
+ This way it is possible to see how the app behaves.
+*/
+
+import fakeData from '../utils/fakedata.json';
+
+export const fetchAllSeries = next => {
+  next(fakeData.series.sort((a, b) => a.name > b.name));
+  /* fetch('http://heartbeats.dk/wp-json/wp/v2/series?parent=1284&per_page=100')
     .then(response => response.json())
     .then(json => {
       next(json);
-    }
-  );
+    });
+    */
 };
 
 // Episodes
 export const fetchEpisodes = (seriesId, next) => {
+  const episodes = fakeData.episodes.filter(e => e.seriesId === seriesId);
+  next(episodes);
+  /*
   fetch(`http://heartbeats.dk/wp-json/wp/v2/podcast?series=${seriesId}&per_page=100`)
     .then(response => response.json())
     .then(json => {
       next(json);
-    }
-    );
+    });
+    */
 };
